1d02527ec41c924bbeb45afefaa44369b3166e20
AugChao <august.chao@twsc.io>
2024-09-26 06:23:33 +0000

    update process

---
+++ base-os/install-docker-compose.sh
@@ -0,0 +1,4 @@
+sudo apt install -y vim git python3-dev cython3 python3-venv
+#python3 -m venv .venv
+#python3 -m pip install -U pip setuptools
+#pip3 install "cython<3.0.0" wheel && pip install pyyaml==5.4.1 --no-build-isolation
